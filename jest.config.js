6384eae00286dc2986fefdb46d2881ffa39e1808
Yvann Boucher <yvann.boucher@gmail.com>
2023-01-12 18:16:41 +0400

    WIP: setup dev-env

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+export default {
+  testMatch: ['<rootDir>/src/**/*.test.ts'],
+  moduleNameMapper: { '^(\\.{1,2}/.*)\\.js$': '$1' },
+  extensionsToTreatAsEsm: ['.ts'],
+  transform: {
+    '^.+\\.ts?$': [
+      '@swc/jest',
+      {
+        jsc: {
+          parser: { syntax: 'typescript', decorators: true },
+          target: 'es2022',
+        },
+      },
+    ],
+  },
+};
